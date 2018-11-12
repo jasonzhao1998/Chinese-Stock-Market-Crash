a68797da24ef80e10b81e103e4077e28e6320567
Jason Zhao <jasonyz@umich.edu>
2018-11-12 17:13:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import csv
 import os
 
 # change file name, only in csv format
-fileName = '600111'
+fileName = '600010'
 
 # initialize data lists
 dates = []
@@ -12,7 +12,7 @@ closingPrices = []
 highestPrices = []
 lowestPrices = []
 percentGain = []
-percentShortSale = []
+percentShortsell = []
 percentMargin = []
 
 with open(fileName + '.csv', 'r') as csvfile:
@@ -27,15 +27,30 @@ with open(fileName + '.csv', 'r') as csvfile:
     highestPrices.append(float(row[11]))
     lowestPrices.append(float(row[12]))
     
+    # margin trading percentage
+    if float(row[19]) == 0:
+      percentMargin.append(0)
+    else:
+      percentMargin.append(float(row[2]) / float(row[19]))
+    
+    # shortsell percentage
+    if float(row[18]) == 0:
+      percentShortsell.append(0)
+    else:
+      percentShortsell.append(float(row[5]) / float(row[18]))
+    
     # 0 percent gain for empty entry
     if len(row[16]):
-      percentGain.append(float(row[16]))
+      percentGain.append(100 * float(row[16]))
     else:
       percentGain.append(0)
 
 # create directory if does not exist
 if not os.path.exists(fileName):
   os.mkdir(fileName)
+
+# resizes plots
+plt.rcParams["figure.figsize"] = [24, 5]
 
 plt.plot(dates, closingPrices)
 plt.xlabel('Dates')
@@ -58,7 +73,7 @@ plt.title('Lowest Prices')
 plt.savefig(fileName + '/lowestPrices.png')
 plt.clf()
 
-plt.plot(dates, percentGain)
+plt.plot(dates, openingPrices)
 plt.xlabel('Dates')
 plt.ylabel('Prices')
 plt.title('Opening Prices')
@@ -70,4 +85,18 @@ plt.xlabel('Dates')
 plt.ylabel('Percent Gain')
 plt.title('Percent Gain')
 plt.savefig(fileName + '/percentGain.png')
+plt.clf()
+
+plt.plot(dates, percentShortsell)
+plt.xlabel('Dates')
+plt.ylabel('Percent Shortsell')
+plt.title('Percent Shortsell')
+plt.savefig(fileName + '/percentShortsell.png')
+plt.clf()
+
+plt.plot(dates, percentMargin)
+plt.xlabel('Dates')
+plt.ylabel('Percent Margin')
+plt.title('Percent Margin')
+plt.savefig(fileName + '/percentMargin.png')
 plt.clf()
