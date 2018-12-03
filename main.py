e8a9f0f60ac61575252fa2b89f768433f9cc2737
Jason Zhao <jasonyz@umich.edu>
2018-12-03 16:18:00 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,226 +1,114 @@
+import os
+import csv
+import datetime
+import numpy as np
+import matplotlib.dates as dt
 import matplotlib.pyplot as plt
 
-import datetime
-
-import matplotlib.dates as dt
-
-import csv
-
-import os
-
 # change file name, only in csv format
+FILE_NAME = '600111'
+
+
+def plot(x, y, x_label, y_label, title, log=False):
+    if log:
+        plt.yscale('log')
+    plt.plot(x, y)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.title(title)
+    plt.savefig(FILE_NAME + '/' + title + '.png')
+    plt.clf()
+
+
+def read_data():
+    # initialize lists
+    dates = []
+    remaining_margin = []
+    margin_buy = []
+    margin_repay = []
+    remaining_shortsell = []
+    short_sell = []
+    short_repay = []
+    closing_prices = []
+    percent_gain = []
+    turnover_rate = []
+    trading_volume = []
+    turnover = []
+
+    with open(FILE_NAME + '.csv', 'Ur') as csv_file:
+        data = csv.reader(csv_file, delimiter=',')
+        for row in data:
+            if row[8] == 'date':  # skip first row
+                continue
+
+            # date
+            cur_date = row[8].split('-')
+            cur_date = [int(i) for i in cur_date]
+            if cur_date[0] < 2014 or cur_date[0] > 2015:
+                continue
+            elif cur_date[0] == 2014 and cur_date[1] < 10:
+                continue
+            dates.append(datetime.datetime(cur_date[0], cur_date[1], cur_date[2]))
+
+            # all other data
+            remaining_margin.append(float(row[2]))
+            margin_buy.append(float(row[3]))
+            margin_repay.append(float(row[4]))
+            remaining_shortsell.append(float(row[5]))
+            short_sell.append(float(row[6]))
+            short_repay.append(float(row[7])) if float(row[7]) != 0 else short_repay.append(0.1)
+            closing_prices.append(float(row[10]))
+            percent_gain.append(float(row[16]))
+            turnover_rate.append(float(row[17]))
+            trading_volume.append(float(row[18]))
+            turnover.append(float(row[19]))
+
+    return {
+        "dates": dates,
+        "remaining margin": np.array(remaining_margin, dtype=np.float),
+        "margin buy": np.array(margin_buy, dtype=np.float),
+        "margin repay": np.array(margin_repay, dtype=np.float),
+        "remaining shortsell": np.array(remaining_shortsell, dtype=np.float),
+        "short sell": np.array(short_sell, dtype=np.float),
+        "short repay": np.array(short_repay, dtype=np.float),
+        "closing prices": np.array(closing_prices, dtype=np.float),
+        "percent gain": np.array(percent_gain, dtype=np.float),
+        "turnover rate": np.array(turnover_rate, dtype=np.float),
+        "trading volume": np.array(trading_volume, dtype=np.float),
+        "turnover": np.array(turnover, dtype=np.float)
+    }
+
+
+def save_plots(data):
+    # create directory if does not exist
+    if not os.path.exists(FILE_NAME):
+        os.mkdir(FILE_NAME)
+
+    # resize plots
+    plt.rcParams["figure.figsize"] = [25, 6]
+
+    plot(data["dates"], data["closing prices"], "Dates", "Prices in yuan", "Closing Prices")
+    plot(data["dates"], data["percent gain"], "Dates", "Percent gain", "Percent Gain")
+
+    # margin trading plots
+    plot(data["dates"], data["margin buy"] / data["margin repay"], "Dates", "Ratio", "Margin Buy Over Margin Repay")
+    plot(data["dates"], data["margin buy"] / data["turnover"], "Dates", "Ratio", "Margin Buy Over Turnover")
+    plot(data["dates"], data["margin repay"] / data["turnover"], "Dates", "Ratio", "Margin Repay Over Turnover")
+
+    # short selling plots
+    plot(data["dates"], data["short sell"] / data["short repay"], "Dates",
+         "Ratio", "Short Sell Over Short Repay", log=True)
+    plot(data["dates"], data["short sell"] / data["trading volume"], "Dates", "Ratio", "Short Sell Over Trading Volume")
+    plot(data["dates"], data["short repay"] / data["trading volume"], "Dates",
+         "Ratio", "Short Repay Over Trading Volume")
+
+    # delay plots
+
+
+def main():
+    data = read_data()
+    save_plots(data)
 
-fileName = '600010'
 
-# initialize data lists
-
-dates = []
-
-closingPrices = []
-
-percentGain = []
-
-percentShortsell = []
-
-percentMargin = []
-
-ratioShortsell = []
-
-ratioMargin = []
-
-with open(fileName + '.csv', 'r') as csvfile:
-    plots = csv.reader(csvfile, delimiter=',')
-
-    for row in plots:
-
-        if row[8] == 'date':  # skip first row
-
-            continue
-
-        # date
-
-        curDate = row[8].split('-')
-
-        curDate = [int(i) for i in curDate]
-
-        if curDate[0] < 2014 or curDate[0] > 2015:
-
-            continue
-
-        elif curDate[0] == 2014 and curDate[1] < 10:
-
-            continue
-
-        dates.append(datetime.datetime(curDate[0], curDate[1], curDate[2]))
-
-        # closing prices
-
-        closingPrices.append(float(row[10]))
-
-        # 0 percent gain for empty entry
-
-        if len(row[16]):
-
-            percentGain.append(float(row[16]))
-
-        else:
-
-            percentGain.append(0)
-
-        # shortsell percentage
-
-        if float(row[18]) == 0:
-
-            percentShortsell.append(0)
-
-        else:
-
-            percentShortsell.append(500 * float(row[5]) / float(row[18]))
-
-        # margin trading percentage
-
-        if float(row[19]) == 0:
-
-            percentMargin.append(0)
-
-        else:
-
-            percentMargin.append(float(row[2]) / float(row[19]))
-
-        # shortsell ratio
-
-        if float(row[7]) == 0:
-
-            ratioShortsell.append(0)
-
-        else:
-
-            rate = float(row[6]) / float(row[7])
-
-            ratioShortsell.append(rate)
-
-        # margin trading ratio
-
-        if float(row[4]) == 0:
-
-            ratioMargin.append(0)
-
-        else:
-
-            ratioMargin.append(float(row[3]) / float(row[4]))
-
-# create directory if does not exist
-
-if not os.path.exists(fileName):
-    os.mkdir(fileName)
-
-# resize plots
-
-plt.rcParams["figure.figsize"] = [25, 6]
-
-plt.plot(dates, closingPrices)
-
-plt.xlabel('Dates')
-
-plt.ylabel('Prices')
-
-plt.title('Closing Prices')
-
-plt.savefig(fileName + '/closingPrices.png')
-
-plt.clf()
-
-plt.plot(dates, percentGain)
-
-plt.xlabel('Dates')
-
-plt.ylabel('Percent Gain')
-
-plt.title('Percent Gain')
-
-plt.savefig(fileName + '/percentGain.png')
-
-plt.clf()
-
-plt.plot(dates, percentShortsell)
-
-plt.xlabel('Dates')
-
-plt.ylabel('Percent Shortsell')
-
-plt.title('Percent Shortsell')
-
-plt.savefig(fileName + '/percentShortsell.png')
-
-plt.clf()
-
-plt.plot(dates, percentMargin)
-
-plt.xlabel('Dates')
-
-plt.ylabel('Percent Margin')
-
-plt.title('Percent Margin')
-
-plt.savefig(fileName + '/percentMargin.png')
-
-plt.clf()
-
-plt.yscale('log')
-
-plt.plot(dates, ratioShortsell)
-
-plt.xlabel('Dates')
-
-plt.ylabel('Ratio Shortshell')
-
-plt.title('Ratio Shortshell')
-
-plt.savefig(fileName + '/ratioShortsell.png')
-
-plt.clf()
-
-plt.plot(dates, ratioMargin)
-
-plt.xlabel('Dates')
-
-plt.ylabel('Ratio Margin')
-
-plt.title('Ratio Margin')
-
-plt.savefig(fileName + '/ratioMargin.png')
-
-plt.clf()
-
-
-plt.plot(dates, percentMargin, '-b', label='% Margin')
-plt.plot(dates, closingPrices, '-r', label='Closing Prices')
-
-plt.xlabel('Dates')
-
-plt.ylabel('% Margin & Closing Prices')
-
-plt.title('% Margin vs. Closing Prices')
-
-plt.legend(loc='upper left')
-
-plt.savefig(fileName + '/marginVsClosingPrice.png')
-
-plt.clf()
-
-
-
-plt.plot(dates, percentShortsell, '-b', label='% Short Sell')
-plt.plot(dates, closingPrices, '-r', label='Closing Prices')
-
-plt.xlabel('Dates')
-
-plt.ylabel('% Short & Closing Prices')
-
-plt.title('% Short vs. Closing Prices')
-
-plt.legend(loc='upper left')
-
-plt.savefig(fileName + '/shortVsClosingPrice.png')
-
-plt.clf()
+if __name__ == '__main__':
+    main()
